186ab25785b134fcad8867da0742d30db06ddf35
jiahangmao <75797567+jiahangmao@users.noreply.github.com>
2021-01-09 00:23:14 +0800

    Update core.js

--- js/core.js
+++ js/core.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     readmeURL = 'https://raw.githubusercontent.com/' + user + '/' + user + '.github.io/master/About Me.md';
 
 
-    $("#header").text("思想监狱");
+    $("#header").text("好像说了很多, 却又好像什么都没说");
     $("#commentsList").removeAttr('data_comments_url');
     $("#tips").html("我们不会获取您的用户名和密码,评论直接通过 HTTPS 与 Github API交互,<br>如果您开启了两步验证,请在博客的<a  target=\"_blank\" href=\"" + issuesHTML + "\">Github issues</a>下添加 Comment");
 
